7699340640b85db704a86cedcbab7f5832cb0a81
dadaying <2567077650@qq.com>
2022-08-06 23:48:34 +0800

    [experiment & common & doc] 1.add experiment; 2.add common; 3.add project_doc

---
+++ experiment/dadaying/test.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World\n";
+    std::cout << "__clang__: " << __clang__ << std::endl;
+    std::cout << "__cplusplus: " << __cplusplus << std::endl;
+    return 0;
+}
