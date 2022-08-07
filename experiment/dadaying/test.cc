5eaea7b650f8c0222fcc3eddafa34b86cc19b89b
dadaying <2567077650@qq.com>
2022-08-07 23:18:08 +0800

    [doc] Compile C++17 with clang in vacode

--- experiment/dadaying/test.cc
+++ experiment/dadaying/test.cc
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <vector>
 
 int main() {
     std::cout << "Hello World\n";
     std::cout << "__clang__: " << __clang__ << std::endl;
     std::cout << "__cplusplus: " << __cplusplus << std::endl;
+    const std::vector<int> vec_nums(3, 1);
+    std::cout << "vec nums:\n";
+    for (int i = 0; i < vec_nums.size(); ++i) {
+        std::cout << "i = " << i << ", num is:" << vec_nums[i] << "\n";
+    }
+    std::cout << "\n";
     return 0;
 }
